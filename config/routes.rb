f4bb0e4016d0fce7b2da1813382697b89316fd54
Lawrence Xia <xialairei@gmail.com>
2014-05-27 16:00:24 -0400

    add mike and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
    root 'index#index'
    get 'contact' => 'index#contact'
+   get 'contact' => 'index#about'
    get 'waddlewar' => 'index#waddlewar'
    get 'superfrogjump' => 'index#superfrogjump'
    get 'post_score' => 'high_score#post_score'
